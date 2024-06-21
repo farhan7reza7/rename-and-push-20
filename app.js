24f677b1f7e5f9e77071159cee20bd8688d4846b
Farhan Reza <fr7reza@gmail.com>
2024-06-21 19:34:31 +0530

    init correctly done

--- app.js
+++ app.js
@@ -468,8 +468,8 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-app.listen(5000, () => {
+/*app.listen(5000, () => {
   console.log(`server listen on http://localhost:5000`);
-});
+});*/
 
 module.exports = app;
