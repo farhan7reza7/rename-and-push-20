0171d94de7195dffd9aef7520b23f9c37d2267e7
Farhan Reza <fr7reza@gmail.com>
2024-07-01 21:52:33 +0530

    ok

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  //res.render('index', { title: 'Express' });
+  res.send({ title: "Express" });
 });
 
 module.exports = router;
